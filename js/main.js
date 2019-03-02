523067d8f00c82a8fb582ea8789f6b6045854185
tyun <ting_yun@bmc.com>
2019-03-02 07:32:01 -0800

    corrected build script & add pictures taken by Rachael

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-$('.carousel_item').on('mouseenter', function(event) {
-  $(event.target).addClass('active');
-});
-$('.carousel_item').on('mouseleave', function(event) {
-  $(event.target).removeClass('active');
+$(document).ready(function(){
+  $("#gallery").unitegallery();
 });
