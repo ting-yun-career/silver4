fff59d289886a13bfd76a1e174a40c587037f644
tyun <ting_yun@bmc.com>
2019-03-02 10:20:07 -0800

    getting gallery and carousel working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
+  $("#carousel").unitegallery({
+    gallery_width: '100%',
+    gallery_min_width: '100%',
+    gallery_height: 600,
+    gallery_min_height: 600,
+    gallery_theme: 'default',
+    slider_scale_mode: 'fit',
+    slider_transition: "fade",
+    slider_transition_speed: 1500,
+    gallery_autoplay: true,
+    gallery_play_interval: 6000,
+  });
   $("#thumbnail-container").unitegallery({
     gallery_width: 900,
-    gallery_min_width: '80%'
+    gallery_min_width: '80%',
+    gallery_theme: 'tiles',
   });
 });
